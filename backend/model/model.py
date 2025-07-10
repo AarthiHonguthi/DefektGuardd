58c6ca9c97d8fa3e2162fc0b77e4e74da36a44ee
MOHAMMED BILAL <bilalmohammed91298@gmail.com>
2025-07-11 00:10:40 +0530

    Updated the model.py

--- backend/model/model.py
+++ backend/model/model.py
@@ -1,33 +1,32 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import os
+from tensorflow.keras.preprocessing.image import img_to_array
 
-MODEL_PATH = "model/detection_model.h5"
 IMG_SIZE = (256, 256)
-CLASS_NAMES = ['intact', 'damaged'] 
+CLASS_NAMES = ['intact', 'damaged']
+MODEL_PATH = "model/resnet34_model.h5"
 
 model = tf.keras.models.load_model(MODEL_PATH)
 
-current_dir = os.path.dirname(__file__)  
-image_path = os.path.join(current_dir, "damagedfoodpackagingbox10.jpeg")
 def preprocess_image(image):
     image = image.resize(IMG_SIZE)
-    image = img_to_array(image)         
+    image = img_to_array(image)          
     image = tf.convert_to_tensor(image, dtype=tf.float32)
-    image = tf.expand_dims(image, axis=0)
-    image = image / 255.0
+    image = tf.expand_dims(image, axis=0)  
+    image = image / 255.0                  
     return image
-    # img = load_img(image_path, target_size=(256, 256))      
-    # x = img_to_array(img)                                  
-    # x = np.expand_dims(x, axis=0)                           
-    # x = x / 255.0    
-    # return x  
+    
+def predict(pil_image):
+    try:
+        processed = preprocess_image(pil_image)
+        prediction = model.predict(processed)[0]
+        print("🧠 Raw model output:", prediction.tolist())
 
-def predict(file):
-    processed = preprocess_image(file)
-    prediction = model.predict(processed)[0]
-    print("🧠 Raw model output:", prediction.tolist())
-    label = CLASS_NAMES[np.argmax(prediction)]
-    confidence = float(np.max(prediction))
-    return {"label": label, "confidence": round(confidence, 4)}
+        label_index = np.argmax(prediction)
+        label = CLASS_NAMES[label_index]
+        confidence = float(np.max(prediction))
+
+        return {"label": label, "confidence": round(confidence, 4)}
+    except Exception as e:
+        print("Prediction error:", str(e))
+        return {"error": str(e)}
